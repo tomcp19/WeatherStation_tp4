ff1fba3d863338196ed8ca9b7451297493c68a52
Tom <tomcp19@gmail.com>
2020-11-15 09:39:26 -0500

    1 - refait les dépendances

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using WeatherApp.ViewModels;
 
 namespace OpenWeatherAPITests
 {
     public class OpenWeatherProcessorTests
     {
+        TemperatureViewModel _sut = new TemperatureViewModel();
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException() 
+        { 
+           // _sut.GetOneCall
+        }
+
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException() 
+        { 
+
+        }
+
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException() 
+        { 
+
+        }
+
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException() 
+        { 
+
+        }
+
+
+
 
     }
 }
